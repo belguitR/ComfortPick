daf6b3716c64aff60f4d3dda2a58d52854a3418a
belguitR <belguithrami150@gmail.com>
2026-05-06 04:06:35 +0200

    modernize test mocking setup

--- backend/src/test/kotlin/com/comfortpick/api/summoner/MatchImportControllerIntegrationTest.kt
+++ backend/src/test/kotlin/com/comfortpick/api/summoner/MatchImportControllerIntegrationTest.kt
@@ -26,9 +26,9 @@ import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
 import java.time.Instant
@@ -56,13 +56,13 @@ class MatchImportControllerIntegrationTest {
     @Autowired
     private lateinit var personalMatchupStatsRepository: PersonalMatchupStatsRepository
 
-    @MockBean
+    @MockitoBean
     private lateinit var riotApiPort: RiotApiPort
 
-    @SpyBean
+    @MockitoSpyBean
     private lateinit var playerMatchupExtractor: PlayerMatchupExtractor
 
-    @SpyBean
+    @MockitoSpyBean
     private lateinit var recalculatePersonalMatchupStatsUseCase: RecalculatePersonalMatchupStatsUseCase
 
     @BeforeEach
--- backend/src/test/kotlin/com/comfortpick/api/summoner/SummonerControllerIntegrationTest.kt
+++ backend/src/test/kotlin/com/comfortpick/api/summoner/SummonerControllerIntegrationTest.kt
@@ -15,8 +15,8 @@ import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import java.time.LocalDateTime
@@ -32,7 +32,7 @@ class SummonerControllerIntegrationTest {
     @Autowired
     private lateinit var riotAccountRepository: RiotAccountRepository
 
-    @MockBean
+    @MockitoBean
     private lateinit var riotApiPort: RiotApiPort
 
     @BeforeEach
